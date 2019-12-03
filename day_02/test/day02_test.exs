c9505e2cabc960eaa11fa8a05c83c131fa6a4ab0
Tim Viaene <tim.viaene@gmail.com>
2019-12-03 21:27:37 +0100

    IntCode passing tests

--- day_02/test/day02_test.exs
+++ day_02/test/day02_test.exs
@@ -8,19 +8,23 @@ defmodule IntCodeTest do
   doctest IntCode
 
   test "Simplest add operation" do
-    assert IntCode.process("1,1,1,0") == "1,1,1,2"
+    assert IntCode.process("1,1,1,3,99") == "1,1,1,2,99"
   end
 
-  # test "Simplest multiply operation" do
-  #   assert IntCode.process("2,1,1,0") == "2,1,1,1"
-  # end
+  test "Two add operations" do
+    assert IntCode.process("1,1,1,3,1,1,8,7,99") == "1,1,1,2,1,1,8,100,99"
+  end
 
-  # test "Basic programs" do
-  #   assert IntCode.process("1,9,10,3,2,3,11,0,99,30,40,50") == "3500,9,10,70,2,3,11,0,99,30,40,50"
-  #   assert IntCode.process("1,0,0,0,99") == "2,0,0,0,99"
-  #   assert IntCode.process("2,3,0,3,99") == "2,3,0,6,99"
-  #   assert IntCode.process("2,4,4,5,99,0") == "2,4,4,5,99,9801"
-  #   assert IntCode.process("1,1,1,4,99,5,6,0,99") == "30,1,1,4,2,5,6,0,99"
-  # end
+  test "Simplest multiply operation" do
+    assert IntCode.process("2,1,1,3,99") == "2,1,1,1,99"
+  end
+
+  test "Basic programs" do
+    assert IntCode.process("1,9,10,3,2,3,11,0,99,30,40,50") == "3500,9,10,70,2,3,11,0,99,30,40,50"
+    assert IntCode.process("1,0,0,0,99") == "2,0,0,0,99"
+    assert IntCode.process("2,3,0,3,99") == "2,3,0,6,99"
+    assert IntCode.process("2,4,4,5,99,0") == "2,4,4,5,99,9801"
+    assert IntCode.process("1,1,1,4,99,5,6,0,99") == "30,1,1,4,2,5,6,0,99"
+  end
 
 end
