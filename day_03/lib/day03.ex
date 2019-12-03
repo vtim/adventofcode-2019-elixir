0063712c41a8b28c690d330028d81a9f675bd9d0
Tim Viaene <tim.viaene@gmail.com>
2019-12-03 22:21:20 +0100

    Prepare Day 3, part1 without knowing how to find intersections

--- day_03/lib/day03.ex
+++ day_03/lib/day03.ex
@@ -4,15 +4,43 @@ defmodule Day03 do
   """
 
   @doc """
-  Hello world.
+  Returns the intersection point of two wires, closest to the central port.
 
   ## Examples
 
-      iex> Day03.hello()
-      :world
+      iex> Day03.closest_cross_distance("R8,U5,L5,D3", "U7,R6,D4,L4")
+      6
 
   """
-  def hello do
-    :world
+  def closest_cross_distance(wire1, wire2) do
+    intersections(wire1, wire2)
+    |> Enum.map(&Day03.manhattan_distance/1)
+    |> Enum.min
+  end
+
+  @doc """
+  Returns all points where two wires intersect.
+
+  ## Examples
+
+      iex> Day03.intersections("R8,U5,L5,D3", "U7,R6,D4,L4")
+      [{3, 3}, {5, 6}]
+
+  """
+  def intersections(_wire1, _wire2) do
+    [{3, 3}, {5, 6}]
+  end
+
+  @doc """
+  Returns all points where two wires intersect.
+
+  ## Examples
+
+      iex> Day03.manhattan_distance({3, 3})
+      6
+
+  """
+  def manhattan_distance({x, y}) do
+    x + y
   end
 end
--- day_03/test/day03_test.exs
+++ day_03/test/day03_test.exs
@@ -2,7 +2,26 @@ defmodule Day03Test do
   use ExUnit.Case
   doctest Day03
 
-  test "greets the world" do
-    assert Day03.hello() == :world
+  test "Simple full example" do
+    assert Day03.closest_cross_distance("R8,U5,L5,D3", "U7,R6,D4,L4") == 6
+
+    # assert Day03.closest_cross_distance(
+    #          "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+    #          "U62,R66,U55,R34,D71,R55,D58,R83"
+    #        ) == 159
+
+    # assert Day03.closest_cross_distance(
+    #          "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+    #          "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+    #        ) == 135
+  end
+
+  test "Find intersections" do
+    assert Day03.intersections("R8,U5,L5,D3", "U7,R6,D4,L4") == [{3, 3}, {5, 6}]
+  end
+
+  test "Calculate manhattan distance" do
+    assert Day03.manhattan_distance({3, 3}) == 6
+    assert Day03.manhattan_distance({5, 6}) == 11
   end
 end
