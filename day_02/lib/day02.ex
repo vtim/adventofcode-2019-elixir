cb6c258110f17c7ec58f0dd44dc1c18065c0e510
Tim Viaene <tim.viaene@gmail.com>
2019-12-03 14:22:13 +0100

    Start day 2 implementation

--- day_02/lib/day02.ex
+++ day_02/lib/day02.ex
@@ -16,3 +16,22 @@ defmodule Day02 do
     :world
   end
 end
+
+defmodule IntCode do
+  @doc """
+  Process an Intcode program.
+
+  ## Examples
+
+    iex> IntCode.process("1,1,1,0")
+    "1,1,1,2"
+  """
+  @spec process(binary) :: binary
+  def process(program) do
+    program
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+    |> List.replace_at(3, 2)
+    |> Enum.join(",")
+  end
+end
--- day_02/test/day02_test.exs
+++ day_02/test/day02_test.exs
@@ -1,8 +1,26 @@
 defmodule Day02Test do
   use ExUnit.Case
   doctest Day02
+end
 
-  test "greets the world" do
-    assert Day02.hello() == :world
+defmodule IntCodeTest do
+  use ExUnit.Case
+  doctest IntCode
+
+  test "Simplest add operation" do
+    assert IntCode.process("1,1,1,0") == "1,1,1,2"
   end
+
+  # test "Simplest multiply operation" do
+  #   assert IntCode.process("2,1,1,0") == "2,1,1,1"
+  # end
+
+  # test "Basic programs" do
+  #   assert IntCode.process("1,9,10,3,2,3,11,0,99,30,40,50") == "3500,9,10,70,2,3,11,0,99,30,40,50"
+  #   assert IntCode.process("1,0,0,0,99") == "2,0,0,0,99"
+  #   assert IntCode.process("2,3,0,3,99") == "2,3,0,6,99"
+  #   assert IntCode.process("2,4,4,5,99,0") == "2,4,4,5,99,9801"
+  #   assert IntCode.process("1,1,1,4,99,5,6,0,99") == "30,1,1,4,2,5,6,0,99"
+  # end
+
 end
