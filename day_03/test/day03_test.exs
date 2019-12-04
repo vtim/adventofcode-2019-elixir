44e457ae1cd3b155a119a5cb32a5c1d0179c07a5
Tim Viaene <tim.viaene@gmail.com>
2019-12-04 19:54:09 +0100

    Day 3, part 2 solved

--- day_03/test/day03_test.exs
+++ day_03/test/day03_test.exs
@@ -24,6 +24,20 @@ defmodule Day03Test do
     assert Day03.manhattan_distance({3, 3}) == 6
     assert Day03.manhattan_distance({5, 6}) == 11
   end
+
+  test "find intersection that is reachable by lowest amount of combined steps" do
+    assert Day03.lowest_steps_to_intersection("R8,U5,L5,D3", "U7,R6,D4,L4") == 30
+
+    assert Day03.lowest_steps_to_intersection(
+             "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+             "U62,R66,U55,R34,D71,R55,D58,R83"
+           ) == 610
+
+    assert Day03.lowest_steps_to_intersection(
+             "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+             "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+           ) == 410
+  end
 end
 
 defmodule WireTest do
@@ -37,7 +51,14 @@ defmodule WireTest do
     assert Wire.execute_command("R1", {1, 1}) == [{2, 1}]
 
     assert Wire.execute_command("U3", {0, 1}) == [{0, 4}, {0, 3}, {0, 2}]
-    assert Wire.execute_command("U5", {10, 10}) == [{10, 15}, {10, 14}, {10, 13}, {10, 12}, {10, 11}]
+
+    assert Wire.execute_command("U5", {10, 10}) == [
+             {10, 15},
+             {10, 14},
+             {10, 13},
+             {10, 12},
+             {10, 11}
+           ]
   end
 
   test "build command" do
