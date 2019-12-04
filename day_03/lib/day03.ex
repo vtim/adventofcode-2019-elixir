cd68f1af4b03b794727fd6fc6ba9726e519a323b
Tim Viaene <tim.viaene@gmail.com>
2019-12-04 13:07:09 +0100

    Passing tests for part 1

--- day_03/lib/day03.ex
+++ day_03/lib/day03.ex
@@ -15,7 +15,7 @@ defmodule Day03 do
   def closest_cross_distance(wire1, wire2) do
     intersections(wire1, wire2)
     |> Enum.map(&Day03.manhattan_distance/1)
-    |> Enum.min
+    |> Enum.min()
   end
 
   @doc """
@@ -24,11 +24,14 @@ defmodule Day03 do
   ## Examples
 
       iex> Day03.intersections("R8,U5,L5,D3", "U7,R6,D4,L4")
-      [{3, 3}, {5, 6}]
+      [{6, 5}, {3, 3}]
 
   """
-  def intersections(_wire1, _wire2) do
-    [{3, 3}, {5, 6}]
+  def intersections(wire1, wire2) do
+    route1 = Wire.build(wire1)
+    route2 = Wire.build(wire2)
+
+    route1 -- (route1 -- route2)
   end
 
   @doc """
@@ -41,6 +44,50 @@ defmodule Day03 do
 
   """
   def manhattan_distance({x, y}) do
-    x + y
+    abs(x) + abs(y)
+  end
+end
+
+defmodule Wire do
+  def build(commands) do
+    commands
+    |> String.split(",")
+    |> Enum.reduce([[{0, 0}]], fn command, acc ->
+      [execute_command(command, hd(hd(acc))) | acc]
+    end)
+    |> List.flatten()
+    |> Enum.reverse()
+    |> tl() # remove origin starting point
+  end
+
+  def execute_command(command, starting_point, route \\ [])
+
+  def execute_command("U0", _, route), do: route
+  def execute_command("D0", _, route), do: route
+  def execute_command("L0", _, route), do: route
+  def execute_command("R0", _, route), do: route
+
+  def execute_command("U" <> steps = _command, {x, y} = _starting_point, route) do
+    steps = String.to_integer(steps)
+    new_point = {x, y + 1}
+    execute_command("U#{steps - 1}", new_point, [new_point | route])
+  end
+
+  def execute_command("D" <> steps = _command, {x, y} = _starting_point, route) do
+    steps = String.to_integer(steps)
+    new_point = {x, y - 1}
+    execute_command("D#{steps - 1}", new_point, [new_point | route])
+  end
+
+  def execute_command("L" <> steps = _command, {x, y} = _starting_point, route) do
+    steps = String.to_integer(steps)
+    new_point = {x - 1, y}
+    execute_command("L#{steps - 1}", new_point, [new_point | route])
+  end
+
+  def execute_command("R" <> steps = _command, {x, y} = _starting_point, route) do
+    steps = String.to_integer(steps)
+    new_point = {x + 1, y}
+    execute_command("R#{steps - 1}", new_point, [new_point | route])
   end
 end
--- day_03/test/day03_test.exs
+++ day_03/test/day03_test.exs
@@ -2,26 +2,45 @@ defmodule Day03Test do
   use ExUnit.Case
   doctest Day03
 
-  test "Simple full example" do
+  test "Full examples" do
     assert Day03.closest_cross_distance("R8,U5,L5,D3", "U7,R6,D4,L4") == 6
 
-    # assert Day03.closest_cross_distance(
-    #          "R75,D30,R83,U83,L12,D49,R71,U7,L72",
-    #          "U62,R66,U55,R34,D71,R55,D58,R83"
-    #        ) == 159
+    assert Day03.closest_cross_distance(
+             "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+             "U62,R66,U55,R34,D71,R55,D58,R83"
+           ) == 159
 
-    # assert Day03.closest_cross_distance(
-    #          "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
-    #          "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
-    #        ) == 135
+    assert Day03.closest_cross_distance(
+             "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+             "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+           ) == 135
   end
 
   test "Find intersections" do
-    assert Day03.intersections("R8,U5,L5,D3", "U7,R6,D4,L4") == [{3, 3}, {5, 6}]
+    assert Day03.intersections("R8,U5,L5,D3", "U7,R6,D4,L4") == [{6, 5}, {3, 3}]
   end
 
   test "Calculate manhattan distance" do
     assert Day03.manhattan_distance({3, 3}) == 6
     assert Day03.manhattan_distance({5, 6}) == 11
+  end
+end
+
+defmodule WireTest do
+  use ExUnit.Case
+  doctest Wire
+
+  test "build simple routes" do
+    assert Wire.execute_command("U1", {0, 0}) == [{0, 1}]
+    assert Wire.execute_command("D1", {1, 1}) == [{1, 0}]
+    assert Wire.execute_command("L1", {1, 1}) == [{0, 1}]
+    assert Wire.execute_command("R1", {1, 1}) == [{2, 1}]
+
+    assert Wire.execute_command("U3", {0, 1}) == [{0, 4}, {0, 3}, {0, 2}]
+    assert Wire.execute_command("U5", {10, 10}) == [{10, 15}, {10, 14}, {10, 13}, {10, 12}, {10, 11}]
+  end
+
+  test "build command" do
+    assert Wire.build("U1,U2") == [{0, 1}, {0, 2}, {0, 3}]
   end
 end
