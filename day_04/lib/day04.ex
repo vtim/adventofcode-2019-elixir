7002d11a029f0e4d4de84f3554b0bfdb07c808a7
Tim Viaene <tim.viaene@gmail.com>
2019-12-05 14:41:11 +0100

    Day 4, parts 1 and 2 solved

--- day_04/lib/day04.ex
+++ day_04/lib/day04.ex
@@ -1,18 +1,86 @@
 defmodule Day04 do
-  @moduledoc """
-  Documentation for Day04.
-  """
+  def part1 do
+    count =
+      264_360..746_325
+      |> Enum.count(fn x -> Password.validate(x) end)
 
-  @doc """
-  Hello world.
+    IO.puts("Part 1: Found #{count} possible passwords")
+  end
 
-  ## Examples
+  def part2 do
+    count =
+      264_360..746_325
+      |> Enum.count(fn x -> Password.validate(x) && Password.validate_part2(x) end)
 
-      iex> Day04.hello()
-      :world
+    IO.puts("Part 2: Found #{count} possible passwords")
+  end
+end
 
-  """
-  def hello do
-    :world
+defmodule Password do
+  def validate(password) do
+    validate_six_digit_number(password) &&
+      validate_range(password, 264_360..746_325) &&
+      validate_adjacent(password) &&
+      validate_never_decrease(password)
+  end
+
+  defp validate_six_digit_number(password) do
+    is_number(password) &&
+      String.length(Integer.to_string(password)) == 6
+  end
+
+  defp validate_range(password, range) do
+    password in range
+  end
+
+  defp validate_adjacent(password) do
+    list1 = Integer.to_charlist(password)
+
+    list2 = Enum.drop(list1, 1)
+
+    pairs =
+      Enum.zip(list1, list2)
+      |> Enum.find(fn {a, b} -> a == b end)
+
+    pairs != nil
+  end
+
+  defp validate_never_decrease(password) do
+    list1 = Integer.to_charlist(password)
+    list2 = Enum.drop(list1, 1)
+
+    pairs =
+      Enum.zip(list1, list2)
+      |> Enum.find(fn {a, b} -> b < a end)
+
+    pairs == nil
+  end
+
+  def validate_part2(password) do
+    password
+    |> Integer.to_charlist()
+    |> Enum.map(fn x -> {x, 1} end)
+    |> sieve()
+    |> Enum.find(fn {_, count} -> count == 2 end) !=
+      nil
+  end
+
+  def sieve(list, index \\ 0)
+  def sieve(list, index) when index == length(list) - 1, do: list
+
+  def sieve(list, index) do
+    {current_value, current_count} = Enum.at(list, index)
+    {next_value, next_count} = Enum.at(list, index + 1)
+
+    if current_value == next_value do
+      new_list =
+        list
+        |> List.delete_at(index + 1)
+        |> List.replace_at(index, {current_value, current_count + next_count})
+
+      sieve(new_list, index)
+    else
+      sieve(list, index + 1)
+    end
   end
 end
--- day_04/test/day04_test.exs
+++ day_04/test/day04_test.exs
@@ -1,8 +1,49 @@
 defmodule Day04Test do
   use ExUnit.Case
   doctest Day04
+end
 
-  test "greets the world" do
-    assert Day04.hello() == :world
+
+defmodule PasswordTest do
+  use ExUnit.Case
+  doctest Password
+
+  test "Password must be 6 digits" do
+    assert Password.validate("12345x") == false
+    assert Password.validate(34445) == false
+    assert Password.validate(344446)
+    assert Password.validate(3234567) == false
+  end
+
+  test "Password must be in given range" do
+    # 264360..746325
+    assert Password.validate(123456) == false
+    assert Password.validate(266660) == false
+    assert Password.validate(266667)
+    assert Password.validate(555555)
+    assert Password.validate(666777)
+    assert Password.validate(77888) == false
+    assert Password.validate(888888) == false
+  end
+
+  test "Two adjacent digits must be the same" do
+    assert Password.validate(555555)
+    assert Password.validate(345677)
+    assert Password.validate(345678) == false
+  end
+
+  test "Going from left to right, the digits never decrease" do
+    assert Password.validate(555555)
+    assert Password.validate(554555) == false
+    assert Password.validate(556789)
+    assert Password.validate(556798) == false
+  end
+
+  test "the two adjacent matching digits are not part of a larger group of matching digits" do
+    assert Password.validate_part2(123444) == false
+    assert Password.validate_part2(123456) == false
+    assert Password.validate_part2(111122)
+    assert Password.validate_part2(112112)
+    assert Password.validate_part2(112222)
   end
 end
