d4cf580517f659a8a0ddd04900ec9f6586be498f
Tim Viaene <tim.viaene@gmail.com>
2019-12-13 10:32:16 +0100

    Solved Day 05 part 2

--- day_05/lib/day05.ex
+++ day_05/lib/day05.ex
@@ -1,5 +1,19 @@
 defmodule Day05 do
   def part1 do
+    IO.puts("Enter 1 as the input below:")
+
+    # This will output 6731945
+    execute()
+  end
+
+  def part2 do
+    IO.puts("Enter 5 as the input below:")
+
+    # This will output 9571668
+    execute()
+  end
+
+  defp execute do
     "input.txt"
     |> File.read!()
     |> IntCode.execute_binary()
