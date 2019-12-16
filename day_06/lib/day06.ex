d3a2643399c1465a45d213abdcdfb9e1f3a20269
Tim Viaene <tim.viaene@gmail.com>
2019-12-16 14:00:14 +0100

    Day 6, part 1 solved

--- day_06/lib/day06.ex
+++ day_06/lib/day06.ex
@@ -1,18 +1,54 @@
 defmodule Day06 do
-  @moduledoc """
-  Documentation for Day06.
-  """
+  def part1 do
+    "input.txt"
+    |> File.read!
+    |> OrbitMap.parse()
+    |> OrbitMap.orbit_count()
+    |> IO.inspect(label: "The checksum is")
 
+    # Output: "The checksum is: 171213"
+  end
+end
+
+defmodule OrbitMap do
   @doc """
-  Hello world.
+  Parses map data into a Map
 
   ## Examples
 
-      iex> Day06.hello()
-      :world
-
+  iex> OrbitMap.parse("COM)B")
+  %{ "B" => "COM" }
   """
-  def hello do
-    :world
+  def parse(map_data) do
+    map_data
+    |> String.split("\n")
+    |> Enum.reduce(%{}, fn x, acc ->
+      case String.split(x, ")") do
+        [around, planet] -> Map.put(acc, planet, around)
+        _ -> acc
+      end
+    end)
+  end
+
+  @doc """
+  Calculates the total number of direct and indirect orbits.
+
+  ## Examples
+
+  iex> OrbitMap.orbit_count(%{"B" => "COM", "C" => "B"})
+  3
+  """
+  def orbit_count(map) do
+    map
+    |> Map.keys()
+    |> Enum.reduce(0, fn x, acc -> acc + traverse(map, x) end)
+  end
+
+  defp traverse(map, x, level \\ 0) do
+    # IO.puts("traverse #{x}, level=#{level}")
+    case parent = Map.fetch!(map, x) do
+      "COM" -> level + 1
+      _ -> traverse(map, parent, level + 1)
+    end
   end
 end
--- day_06/test/day06_test.exs
+++ day_06/test/day06_test.exs
@@ -6,3 +6,54 @@ defmodule Day06Test do
     assert Day06.hello() == :world
   end
 end
+
+defmodule OrbitMapTest do
+  use ExUnit.Case
+  doctest OrbitMap
+
+  test "Example checksum" do
+    assert %{
+             "B" => "COM",
+             "C" => "B",
+             "D" => "C",
+             "E" => "D",
+             "F" => "E",
+             "G" => "B",
+             "H" => "G",
+             "I" => "D",
+             "J" => "E",
+             "K" => "J",
+             "L" => "K"
+           }
+           |> OrbitMap.orbit_count() == 42
+  end
+
+  test "Parse map data" do
+    assert """
+           COM)B
+           B)C
+           C)D
+           D)E
+           E)F
+           B)G
+           G)H
+           D)I
+           E)J
+           J)K
+           K)L
+           """
+           |> OrbitMap.parse() == %{
+             "B" => "COM",
+             "C" => "B",
+             "D" => "C",
+             "E" => "D",
+             "F" => "E",
+             "G" => "B",
+             "H" => "G",
+             "I" => "D",
+             "J" => "E",
+             "K" => "J",
+             "L" => "K"
+           }
+  end
+end
